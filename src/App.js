0cf3e1eb774a1e1110ff6480b0480b1ed3bb777a
JeongJeongIn <devforin@gmail.com>
2025-02-23 00:41:53 +0900

    feat: useState Studty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 
 // image import 
+import React, { useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // 변수 저장
+  let posts = '강남 고기 맛집';
+  
+  // state = [state 데이터, state 데이터 변경 함수]
+  // 자주 바뀌는, 중요한 데이터는 state로 선언
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', ' 강남 우동 맛집']);
 
+  let[title,chagneTitle] = useState(['사진','노래','영화']);
+  let[date,chagneDate] = useState(['2024/02/22','2024/02/23','2024/02/23']);
+  
+
+
+  return (
+    <div className='App'>
+      <div className='black-nav'>
+        <div>개발 Blog</div>
+      </div>
+      <div className='list'>
+          <h3>{글제목[0]}</h3>
+          <p> 2월 23일 발행</p>
+          <hr/>     
+      </div>
+      <div className='list'>
+          <h2>{title[0]}</h2>
+          <h4>{date[0]}</h4>
+          <hr/>     
+      </div>
+      <div className='list'>
+          <h2>{title[1]}</h2>
+          <h4>{date[1]}</h4>
+          <hr/>     
+      </div>
+      v
+      <div className='list'>
+          <h2>{title[2]}</h2>
+          <h4>{date[2]}</h4>
+          <hr/>     
+      </div>
+    </div> 
+  );
+
+}
+export default App;
+/*
   let posts = '일찍 퇴근하기';
   let tests = { color : 'blue', fontSize : '50px' } ;
   let bn = 'black-nav';
@@ -31,6 +75,6 @@ function App() {
       <img src ={ logo }/>
     </div>
   );
-}
 
-export default App;
+}
+*/
